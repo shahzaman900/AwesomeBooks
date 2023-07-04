7a64bf81873b598e37508b2a830b3eb5e543b3b8
sheezu900 <shahzamanlive2@gmail.com>
2023-07-04 11:06:44 +0500

    creating books complted now working on remove books

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
-let books = [];
+let bookstore = [];
 let id = 0;
 
 const form = document.getElementById('form');
 form.addEventListener('submit', callbackFunction);
+
 function callbackFunction(event) {
-    event.preventDefault();
-    const myFormData = new FormData(event.target);
-    let formDataObj = {};
-    myFormData.forEach((value, key) => {
+  event.preventDefault();
+  const myFormData = new FormData(event.target);
+  let formDataObj = {};
+  myFormData.forEach((value, key) => {
     formDataObj[key] = value;
-    });
+  });
   id += 1;
-  formDataObj = {...formDataObj, "id": id}
-  books.push(formDataObj);
-  // displayData();
-  const storedBook = JSON.stringify(books);
- localStorage.setItem('books', storedBook);
-};
-
-books = JSON.parse(localStorage.getItem('books'));
-const bookSection = document.getElementById('books');
-function displayData() {
-     books.forEach((book) => {
-   bookSection.innerHTML += `
-			<p id="title">${book.title}</p>
-			<p id="author">${book.author}</p>
-			<button id='btn'>Remove</button>
-			<hr />`
-});
+  formDataObj = { ...formDataObj, id: id };
+  bookstore.push(formDataObj);
+  const storedBook = JSON.stringify(bookstore);
+  localStorage.setItem('books', storedBook);
+  displayData();
 }
-
-//  window.onload = () => {
-//    displayData();
-// };
-
-// const remove = document.getElementById('books')
-
-// remove.addEventListener('click', (id) => {
-
-// })
-
+function displayData() {
+  books = JSON.parse(localStorage.getItem('books'));
+  const bookSection = document.getElementById('books');
+  bookSection.innerHTML = ''; // Clear the existing content
+  books.forEach((book, index) => {
+    const bookElement = document.createElement('div');
+    bookElement.innerHTML = `
+      <p id="title">${book.title}</p>
+      <p id="author">${book.author}</p>
+      <button class='btn' onclick='removeBook(${index})'>Remove</button>
+      <hr />`;
+    bookSection.appendChild(bookElement);
+  });
+}
+function removeBook(index) {
+  books.splice(index, 1);
+  const storedBook = JSON.stringify(books);
+  localStorage.setItem('books', storedBook);
+  displayData();
+}
+window.onload = () => {
+  displayData();
+};
